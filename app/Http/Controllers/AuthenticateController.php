bbe0298ec85cb5e6292e5b81ef4f9a8404041dcd
Iman Syaefulloh <imansyaefulloh@gmail.com>
2015-07-05 21:17:07 +0700

    initial commit

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\User;
+
+class AuthenticateController extends Controller
+{
+    public function __construct()
+    {
+        // apply all jwt.auth middleware to all methods in this controller
+        // except for then authenticate method.
+        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        // get all users from database and return them
+        $users = User::all();
+        return $users;
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // verify credentials and create a token for the users
+            if (! $token = JWTAuth::attempt($credentials))
+            {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token'));
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if ( ! $user = JWTAuth::parseToken()->authenticate())
+            {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ public/scripts/userController.js
@@ -0,0 +1,51 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('l51-auth')
+        .controller('UserController', UserController);  
+
+    UserController.$inject = ['$http', '$auth', '$rootScope'];
+
+    function UserController($http, $auth, $rootScope) {
+
+        var vm = this;
+        
+        vm.users;
+        vm.error;
+
+        vm.getUsers = function() {
+
+            // This request will hit the index method in the AuthenticateController
+            // on the Laravel side and will return the list of users
+            $http.get('api/authenticate').success(function(users) {
+                vm.users = users;
+            }).error(function(error) {
+                vm.error = error;
+            });
+
+        }
+
+        // We would normally put the logout method in the same
+        // spot as the login method, ideally extracted out into
+        // a service. For this simpler example we'll leave it here
+        vm.logout = function() {
+
+            $auth.logout().then(function() {
+
+                // Remove the authenticated user from local storage
+                localStorage.removeItem('user');
+
+                // Flip authenticated to false so that we no longer
+                // show UI elements dependant on the user being logged in
+                $rootScope.authenticated = false;
+
+                // Remove the current user info from rootscope
+                $rootScope.currentUser = null;
+            });
+        }
+
+    }
+    
+})();
